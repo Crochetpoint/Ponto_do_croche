7e30ea4ee066c7a1cb5d3d8ef2792818459eccb7
Crochetpoint <127104055+Crochetpoint@users.noreply.github.com>
2023-03-06 11:41:14 -0300

    commit inicial

---
+++ assets/js/script2.js
@@ -0,0 +1,417 @@
+let products = {
+  data: [
+    {
+      id: "card-1",
+      productName: "Boné Verde",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe1.jpg",
+      link: "produto.html?id=card-1",
+    },
+    {
+      id: "card-2",
+      productName: "Mestre dos magos",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu_edited.jpg",
+      link: "produto.html?id=card-2",
+    },
+    {
+      id: "card-3",
+      productName: "Pano de Mesa",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor.jpg",
+      link: "produto.html?id=card-3",
+    },
+    {
+      id: "card-4",
+      productName: "Botas de cano alto Amarela",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe2.jpg",
+      link: "produto.html?id=card-4",
+    },
+    {
+      id: "card-5",
+      productName: "Raposa Amigurumi",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu2.jpg",
+      link: "produto.html?id=card-5",
+    },
+    {
+      id: "card-6",
+      productName: "Tapete mulher maravilha",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor1.jpg",
+      link: "produto.html?id=card-6",
+    },
+    {
+      id: "card-7",
+      productName: "Botas de cano alto vermelho",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe3.jpg",
+      link: "produto.html?id=card-7",
+    },
+    {
+      id: "card-8",
+      productName: "Mini bruxa",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu3.jpg",
+      link: "produto.html?id=card-8",
+    },
+    {
+      id: "card-9",
+      productName: "Tapete de Sol no fundo roxo",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor2.jpg",
+      link: "produto.html?id=card-9",
+    },
+    {
+      id: "card-10",
+      productName: "Bikini borboleta Arco-Íris",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe4.jpg",
+      link: "produto.html?id=card-10",
+    },
+    {
+      id: "card-11",
+      productName: "Orixá",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu4.jpg",
+      link: "produto.html?id=card-11",
+    },
+    {
+      id: "card-12",
+      productName: "Tapete para banheiro",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor3.jpg",
+      link: "produto.html?id=card-12",
+    },
+    {
+      id: "card-13",
+      productName: "Bikini borboleta lílas",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe5.jpg",
+      link: "produto.html?id=card-13",
+    },
+    {
+      id: "card-14",
+      productName: "Orixá",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu5.jpg",
+      link: "produto.html?id=card-14",
+    },
+    { 
+      id: "card-15",
+      productName: "Tapete Amarelo",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor4.jpg",
+      link: "produto.html?id=card-15",
+    },
+    {
+      id: "card-16",
+      productName: "Bikini borboleta azul",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe6.jpg",
+      link: "produto.html?id=card-16",
+    },
+    {
+      id: "card-17",
+      productName: "Orixá",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu6.jpg",
+      link: "produto.html?id=card-17",
+    },
+    {
+      id: "card-18",
+      productName: "Pequeno pote de croche",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor5.jpg",
+      link: "produto.html?id=card-18",
+    },
+    {
+      id: "card-19",
+      productName: "Botas de cano alto inverso cinza",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe7.jpg",
+      link: "produto.html?id=card-19",
+    },
+    {
+      id: "card-20",
+      productName: "Coelho da pascoa",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu7.jpg",
+      link: "produto.html?id=card-20",
+    },
+    {
+      id: "card-21",
+      productName: "Pote de Decoração",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor6.jpg",
+      link: "produto.html?id=card-21",
+    },
+    {
+      id: "card-22",
+      productName: "Botas de cano alto inverno brancas",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe8.jpg",
+      link: "produto.html?id=card-22",
+    },
+    {
+      id: "caed-23",
+      productName: "Coelho da pascoa",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu8.jpg",
+      link: "produto.html?id=card-23",
+    },
+    {
+      id: "card-24",
+      productName: "Descanço de copo",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor7.jpg",
+      link: "produto.html?id=card-24",
+
+    },
+    {
+      id: "card-25",
+      productName: "Bota de inverno acizentada",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe9.jpg",
+      link: "produto.html?id=card-25",
+    },
+    {
+      id: "card-26",
+      productName: "Coelho da pascoa",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu9.jpg",
+      link: "produto.html?id=card-26",
+    },
+    {
+      id: "card-27",
+      productName: "Mini vaso de croche",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor8.jpg",
+      link: "produto.html?id=card-27",
+    },
+    {
+      id: "card-28",
+      productName: "Saia curta laranja de verão",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe10.jpg",
+      link: "produto.html?id=card-28",
+    },
+    {
+      id: "card-29",
+      productName: "Urso de Amigurumi",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu10.jpg",
+      link: "produto.html?id=card-29",
+    },
+    {
+      id: "card-30",
+      productName: "Conjunto de panos de cozinha",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor9.jpg",
+      link: "produto.html?id=card-30",
+    },
+    {
+      id: "card-31",
+      productName: "Saia longa de verão",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/clothe11.jpg",
+      link: "produto.html?id=card-31",
+    },
+    {
+      id: "card-32",
+      productName: "Homem Aranha",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu11.jpg",
+      link: "produto.html?id=card-32",
+    },
+    {
+      id: "card-33",
+      productName: "Tapete em formato de peixe para entradas",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor10.jpg",
+      link: "produto.html?id=card-34",
+    },
+    {
+      id: "card-34",
+      productName: "Saia curta florida para verão",
+      category: "Roupas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/roupa.png",
+      link: "produto.html?id=card-34",
+    },
+    {
+      id:"card-35",
+      productName: "Boneca tradicional de Amigurumi",
+      category: "Bonecas()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/amigu12.jpg",
+      link: "produto.html?id=card-35",
+    },
+    {
+      id: "card-36",
+      productName: "Almofada estilizada",
+      category: "Decoração()",
+      price: "Clique no icone abaixo para saber mais sobre para fazer a encomenda",
+      image: "images/acessor11.jpg",
+      link: "produto.html?id=card-36",
+    },
+
+  ],
+};
+
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden initially
+  card.classList.add("card", i.category, "hide");
+  //image div
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+  //img tag
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product name
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+  let price = document.createElement("h6");
+  price.innerText = "" + i.price;
+  container.appendChild(price);
+ //button
+let button = document.createElement("button");
+button.style.backgroundImage = "url('images/pesquisa.jpg')";
+button.style.backgroundRepeat = "no-repeat";
+button.style.backgroundSize = "24px 24px"; // definindo o tamanho da imagem como 24px x 24px
+button.style.backgroundPosition = "center";
+button.style.backgroundColor = "white";
+button.style.padding = "20px";
+button.style.borderRadius = "5px";
+button.style.border = "1px solid green";
+container.appendChild(button);
+
+//add styles on hover
+button.addEventListener("mouseover", function() {
+  button.style.backgroundColor = "green";
+  button.style.color = "white";
+});
+
+//remove styles on hover out
+button.addEventListener("mouseout", function() {
+  button.style.backgroundColor = "white";
+  button.style.color = "green";
+});
+
+//add click event to button
+button.addEventListener("click", function() {
+  window.location.href = i.link;
+});
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+//Initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
+//Activate search button on enter key press
+document.getElementById("search-input").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("search").click();
+  }
+});
